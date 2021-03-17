7f4e21a81b048413f54e8f87f5b3978a7644f651
YaakovHatam <kobieshka@gmail.com>
2021-03-17 18:26:49 +0200

    fixes

---
+++ controllers/contact.ctrl.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+
+})
+
+router.post('/', function (req, res) {
+
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const db = require('./db/db');
-const crud = require('./crud');
+
+// controllers
+const contactCtrl = require('./controllers/contact.ctrl');
 
 // configurations
 const PORT = 14700;
@@ -8,24 +9,8 @@ const app = express();
 
 app.use(express.json());
 
-// my modules imports
-crud.crudFactory({
-    tableName: db.dbFactory('contact')
-});
-crud.crudFactory({
-    tableName: db.dbFactory('company')
-});
-crud.crudFactory({
-    tableName: db.dbFactory('quote')
-});
-
-const contact = crud.getController('contact');
-const company = crud.getController('company');
-const quote = crud.getController('quote');
-
-app.use('/contact', contact);
+app.route('/crm/v1')
 app.use('/company', company);
-app.use('/quote', quote);
 
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
