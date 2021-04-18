3fcc5a4ba591e225e09ce0fc072b49d70dfcba26
YaakovHatam <kobieshka@gmail.com>
2021-04-18 18:59:26 +0300

    fix params

--- server/db/db.js
+++ server/db/db.js
@@ -17,7 +17,7 @@ function performSelect(query, values, callback) {
    });
 }
 
-function performInsert() {
+function performInsert(query, values, callback) {
    connection.query(query, values, function (err, results) {
       callback(err, results);
    });
