589a6dda0105b84c12db2604cd1de58b1e381470
YaakovHatam <kobieshka@gmail.com>
2021-04-18 20:08:42 +0300

    added db install

---
+++ server/install/installation.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+fs = require('fs')
+
+
+fs.readFile('./install/database.sql', 'utf8', function (err, data) {
+   if (err) {
+      return console.log(err);
+   }
+
+   const dbNAme = 'crm2';
+   var connection = mysql.createConnection({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      multipleStatements: true
+   });
+   connection.query(`CREATE DATABASE ${dbNAme} CHARACTER SET utf8 COLLATE utf8_bin`, function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+      connection.changeUser({ database: dbNAme }, function (err) {
+         if (err) throw err;
+         connection.query(data, function (err, res) {
+            console.log(err || res);
+            process.exit(1)
+         })
+      });
+
+
+   });
+
+});
+
